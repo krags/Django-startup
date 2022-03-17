f0e4d047a368ea51d367ff6ceff0c8d2ef67b6cd
Keith Ragsdale <keithrags@gmail.com>
2022-03-16 22:26:17 -0700

    added carasoul

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
+# def home(request):
+#     now = datetime.datetime.now()
+#     html = "<html><body>It is now %s.</body></html>" % now
+#     return HttpResponse(html)
+
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render(request, 'blog/index.html')
