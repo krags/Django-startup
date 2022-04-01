ef93a24050a74777f341ff2e53944de21a753f15
Keith Ragsdale <keithrags@gmail.com>
2022-04-01 09:50:12 -0700

    Moved static url and root variables out of prod and into dev settings

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -15,3 +15,20 @@ DATABASES = {
        'PORT': '5432',
    }
 }
+
+STATICFILES_DIRS = [
+	BASE_DIR / "static", 
+]
+# Static application files.
+STATIC_URL = "/static/"
+
+#Collectstatic collects static files here.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    "./media",
+]
+
+#File uploads location.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
